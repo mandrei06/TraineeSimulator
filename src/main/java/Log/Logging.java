bd2ce5af93bb90e09b6f20bb42d2922f84dad0cd
Usama <usamanavid098@gmail.com>
2022-05-03 16:05:35 +0100

    Logging added

---
+++ src/main/java/Log/Logging.java
@@ -0,0 +1,13 @@
+package Log;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Logging {
+    private static Logger logger = LogManager.getLogger(Logging.class);
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+}
