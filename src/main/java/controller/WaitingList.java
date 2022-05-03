fa68439ec58b2d77f01bac6112a03d201fd0a2ef
maurosorrentino <sorrentino.mauro95@gmail.com>
2022-05-03 23:16:29 +0100

    ManageTrainees WaitingList

---
+++ src/main/java/controller/WaitingList.java
@@ -0,0 +1,31 @@
+package controller;
+
+import model.Trainee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WaitingList {
+    private List<Trainee> javaWaitingList = new ArrayList();
+
+    public List<Trainee> storeJavaWaitingList(Trainee trainee) {
+        try {
+            this.javaWaitingList.add(trainee);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return this.javaWaitingList;
+    }
+
+    public List<Trainee> deleteJavaWaitingList(int javaDevToRemove, List<Trainee> javaWaitingList)
+    {
+        try {
+            for(int i = 0; i <= javaDevToRemove; i++) {
+                javaWaitingList.remove(i);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return javaWaitingList;
+    }
+}
