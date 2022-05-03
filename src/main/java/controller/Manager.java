0f532ecc9dcf031799561039fe077bca3af74430
maurosorrentino <sorrentino.mauro95@gmail.com>
2022-05-03 15:43:35 +0100

    user input completed

--- src/main/java/controller/Manager.java
+++ src/main/java/controller/Manager.java
@@ -6,7 +6,8 @@ public class Manager {
     public void start() {
         try {
             UserInput input = new UserInput();
-            input.askMonths();
+            input.askInput("months");
+            input.askInput("centres");
         } catch (Exception e) {
             e.printStackTrace();
         }
