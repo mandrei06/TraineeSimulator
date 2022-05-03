9fd273f18caed30412894b3297a204c4f0163b2e
Usama <usamanavid098@gmail.com>
2022-05-03 16:55:49 +0100

    changed logging

--- src/main/java/log/Logging.java
+++ src/main/java/log/Logging.java
@@ -2,12 +2,29 @@ package log;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import start.Starter;
 
 public class Logging {
-    private static Logger logger = LogManager.getLogger(Logging.class);
+    private static final Logger logger = LogManager.getLogger(Starter.class);
 
-    public static Logger getLogger() {
-        return logger;
+    public static void traceLog(String message) {
+        logger.trace(message);
     }
+
+    public static void errorLog(String message) {
+        logger.error(message);
+    }
+
+    public static void infoLog(String message) {
+        logger.info(message);
+    }
+
+    public static void debugLog(String message) {
+        logger.debug(message);
+    }
+
+    public static void warnLog(String message){ logger.warn(message);}
+
+    public static void fatalLog(String message){ logger.fatal(message);}
 
 }
