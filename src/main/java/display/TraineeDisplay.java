70da0dc35c99e49e30abbf66ac796ce58c6e0cf2
Muhammad Teladia <P2577087@my365.dmu.ac.uk>
2022-05-04 09:12:32 +0100

    Created Print Statement

---
+++ src/main/java/display/TraineeDisplay.java
@@ -0,0 +1,17 @@
+package display;
+
+public class TraineeDisplay {
+
+    public static void tranieeDisplay(int openCenters, int fullCenters, int numberOfTrainees, int list){
+
+        System.out.println("Number of open centres: " + openCenters +
+                "\nNumber of full centres: " + fullCenters +
+                "\nNumber of trainees currently training: "+ numberOfTrainees +
+                "\nNumber of trainees on the waiting list: "+ list);
+    }
+
+}
+//number of open centres
+//number of full centres
+//number of trainees currently training
+//number of trainees on the waiting list
