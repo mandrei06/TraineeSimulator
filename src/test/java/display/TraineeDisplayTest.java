420093eacaea4de282daee4b5895c4b7617e7f82
mandrei06 <marincasandrei06@gmail.com>
2022-05-05 17:38:34 +0100

    I added some tests

---
+++ src/test/java/display/TraineeDisplayTest.java
@@ -0,0 +1,14 @@
+package display;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TraineeDisplayTest {
+
+
+    @Test
+    public void testGenerate50() {
+        TraineeDisplay.tranieeDisplay(3,2,30,10);
+    }
+}
