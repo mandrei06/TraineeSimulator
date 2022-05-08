c2025d39a01ef6288bf2b74cd89df1607be7e86e
mandrei06 <marincasandrei06@gmail.com>
2022-05-08 19:19:58 +0100

    I added some tests

--- src/test/java/model/TechCentreTest.java
+++ src/test/java/model/TechCentreTest.java
@@ -1,4 +1,4 @@
-/*package model;
+package model;
 
 import junit.framework.Assert;
 import org.junit.jupiter.api.Assertions;
@@ -13,34 +13,24 @@ class TechCentreTest {
 
     @BeforeEach
     public void setUp() {
-        techCentre=new TechCentre(100,false,"java",12);
-        trainee=new Trainee("java");
+        techCentre=new TechCentre(100,false,"java",12,0);
+        trainee=new Trainee("java",0);
     }
 
 
     @Test
     void storeTrainees() {
         techCentre.storeTrainees(trainee);
-        boolean expected= techCentre.getTrainee().equals(trainee);
+        boolean expected= techCentre.getTraineesFromCenter().get(0).equals(trainee);
         Assertions.assertTrue(expected);
     }
 
-    @Test
-    void removeTrainees() {
-        techCentre.storeTrainees(trainee);
-        techCentre.removeTrainees();
-        boolean expected= techCentre.getTechCentreTrainees().isEmpty();
-
-        Assertions.assertTrue(expected);
-    }
 
     @Test
     void getTrainee() {
         techCentre.storeTrainees(trainee);
-        Trainee result=new Trainee("java");
-        result=techCentre.getTrainee();
-        Assert.assertEquals(trainee,result);
+        boolean expected = techCentre.getTraineesFromCenter().size() > 0;
+        Assertions.assertTrue(expected);
     }
 }
 
- */
